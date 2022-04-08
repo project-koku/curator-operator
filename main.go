05bd3b3a3415bcd562b2f5846094d98eb6409b78
skanthed <skanthed@redhat.com>
2022-04-08 15:06:04 -0400

    added postgres library

--- main.go
+++ main.go
@@ -21,6 +21,8 @@ import (
 	"flag"
 	"os"
 
+	_ "github.com/lib/pq"
+
 	// Import all Kubernetes client auth plugins (e.g. Azure, GCP, OIDC, etc.)
 	// to ensure that exec-entrypoint and run can make use of them.
 	_ "k8s.io/client-go/plugin/pkg/client/auth"
